9cf5f5bb9b1151047ce3aa48e47d34e964f9cce6
Surendra Allam <surendra@asterteq.com>
2021-12-27 17:17:56 +0530

    Added project files

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -1,4 +1,4 @@
-"""app URL Configuration
+"""hello_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from upload.views import image_upload
+from webpage.views import home
 
 urlpatterns = [
-    path('', include("web.urls")),
+    path("", image_upload, name="upload"),
+    path("index/", home, name="home"),
     path('admin/', admin.site.urls),
 ]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
